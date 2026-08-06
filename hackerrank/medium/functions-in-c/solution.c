bb0059f39fbbab565be8c9108c2bb682d9189eb9
tamanna-4117 <tamannakumari4117@gmail.com>
2026-08-06 23:45:04 +0530

    ✅ Functions in C | Medium | C | Runtime: N/A | Memory: N/A | 2026-08-06

--- hackerrank/medium/functions-in-c/solution.c
+++ hackerrank/medium/functions-in-c/solution.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include <stdlib.h>
+/*
+Add `int max_of_four(int a, int b, int c, int d)` here.
+*/
+int max_of_four(int a, int b, int c, int d) {
+    
+    int max = a;
+    
+    if (b > max) max = b;
+    if (c > max) max = c;
+    if (d > max) max = d;
+    
+    return max;
+}
 
-int main()
-{
-	int num1, num2;
-    float num3, num4;
-    
-    scanf("%d %d", &num1, &num2);
-    scanf("%f %f", &num3, &num4);
-    
-    printf("%d %d\n", num1 + num2, num1 - num2);
-    printf("%.1f %.1f\n", num3 + num4, num3 - num4);
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
     
     return 0;
 }
